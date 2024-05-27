eee758f6d3cd509e72153bbc8feeade1278efc13
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-05-27 11:43:10 +0700

    fix: code management, put all feature in separate function

--- src/uas-farrel.sh
+++ src/uas-farrel.sh
@@ -1,5 +1,69 @@
 # Code by Farrel Augusta Dinata
 
+show_menu() {
+    echo "===================================================="
+    echo "               --[ Jam : $(date +%T) ]--         "
+    echo $'\033[0;32m-- Selamat datang di menu serbaguna komputer anda -- \033[0m'
+    echo "----------------------------------------------------"
+    echo "Silakan pilih salah satu menu berikut"
+    echo "  1. Lihat path direktori saat ini"
+    echo "  2. Membuat folder baru"
+    echo "  3. Cari sebuah file"
+    echo "  4. Buat catatan"
+    echo "  5. Baca file"
+    echo "  6. Kalkulator"
+    echo "  7. Give me a motivation!"
+    echo "----------------------------------------------------"
+}
+
+make_dir() {
+    read -p "Masukkan nama folder baru: " namaFolder
+    if mkdir $namaFolder; then
+        echo -e "Folder \033[0;33m$namaFolder\033[0m berhasil dibuat."
+    else
+        echo -e "Gagal membuat folder \033[0;33mT$namaFolder\033[0m. Folder mungkin sudah ada."
+    fi
+}
+
+search_file() {
+    read -p "Masukkan nama file yang ingin dicari: " fileToSearch
+    filePath=$(find / -type f -name "$fileToSearch" -print -quit 2> /dev/null)
+    if [ -n "$filePath" ]; then
+        echo -e "\n\033[0;32mFile ditemukan !\033[0m"
+        echo "$filePath"
+    else
+        echo -e "\033[0;32mFile tidak dapat ditemukan !\033[0m"
+    fi
+}
+
+make_note() {
+    read -p "Masukkan nama file: " fileName
+    echo "Masukkan catatan (Ctrl + D to exit)"
+    echo "----------------------------------------------------"
+    cat - > ../sample/notes/$fileName
+    echo "----------------------------------------------------"
+    if [ $? -eq 0 ]; then
+        echo "Catatan berhasil dibuat!"
+    else 
+        echo "Error: catatan gagal dibuat!"
+    fi
+}
+
+read_file() {
+    echo "Daftar file catatan yang sudah dibuat:"
+    ls ../sample/notes > ls_output.txt
+
+    while IFS= read -r line; do
+        echo -e "\e[33m$line\e[0m"
+    done < ls_output.txt
+    echo ""
+
+    read -p "Masukkan catatan yang ingin dibaca: " fileToRead
+    echo ""
+    cat ../sample/notes/$fileToRead
+    rm ls_output.txt
+}
+
 calculator() {
     echo -e "\n===================================================="
     echo "Pilih opsi menu"
@@ -34,22 +98,34 @@ calculator() {
     echo -e "Hasilnya: \033[0;33m$result\033[0m"
 }
 
+give_motivation() {
+    allQuotes=("Don’t compare yourself with anyone in this world. If you do so, you are insulting yourself. - Bill Gates" "Invention requires a long-term willingness to be misunderstood. - Jeff Bezos" "Persistence is very important. You should not give up unless you are forced to give up. - Elon Musk" "The only way to do great work is to love what you do. - Steve Wozniak" "The way to do really big things seems to be to start with deceptively small things.  - Paul Graham")
+
+    currentSecond=$(date +%S)
+    secondModulo5=$((currentSecond % 5))
+    case $secondModulo5 in
+        0)
+            echo -e "\n\033[0;33m${allQuotes[0]}\033[0m"
+            ;;
+        1)
+            echo -e "\n\033[0;33m${allQuotes[1]}\033[0m"
+            ;;
+        2)
+            echo -e "\n\033[0;33m${allQuotes[2]}\033[0m"
+            ;;
+        3)
+            echo -e "\n\033[0;33m${allQuotes[3]}\033[0m"
+            ;; 
+        4)
+            echo -e "\n\033[0;33m${allQuotes[4]}\033[0m"
+            ;;
+    esac
+}
+
 
 ulangiMenu=y
 while [ $ulangiMenu == "y" ]; do
-    echo "===================================================="
-    echo "               --[ Jam : $(date +%T) ]--         "
-    echo $'\033[0;32m-- Selamat datang di menu serbaguna komputer anda -- \033[0m'
-    echo "----------------------------------------------------"
-    echo "Silakan pilih salah satu menu berikut"
-    echo "  1. Lihat path direktori saat ini"
-    echo "  2. Membuat folder baru"
-    echo "  3. Cari sebuah file"
-    echo "  4. Buat catatan"
-    echo "  5. Baca file"
-    echo "  6. Kalkulator"
-    echo "  7. Give me a motivation!"
-    echo "----------------------------------------------------"
+    show_menu
     read -p " >>> " pilihan
 
     case $pilihan in
@@ -57,63 +133,22 @@ while [ $ulangiMenu == "y" ]; do
             pwd
             ;;
         2)
-            read -p "Masukkan nama folder baru: " namaFolder
-            if mkdir $namaFolder; then
-                echo -e "Folder \033[0;33m$namaFolder\033[0m berhasil dibuat."
-            else
-                echo -e "Gagal membuat folder \033[0;33mT$namaFolder\033[0m. Folder mungkin sudah ada."
-            fi
+            make_dir
             ;;
         3)
-            read -p "Masukkan nama file yang ingin dicari: " fileDicari
-            filePath=$(find / -type f -name "$fileDicari" -print -quit 2> /dev/null)
-            if [ -n "$filePath" ]; then
-                echo -e "\n\033[0;32mFile ditemukan !\033[0m"
-                echo "$filePath"
-            else
-                echo -e "\033[0;32mFile tidak dapat ditemukan !\033[0m"
-            fi
+            search_file
             ;;
         4)
-            echo "Masukkan catatan (Ctrl + D to exit)"
-            echo "----------------------------------------------------"
-            cat - > myNotes.txt
-            echo "----------------------------------------------------"
-            if [ $? -eq 0 ]; then
-                echo "Catatan berhasil dibuat!"
-            else 
-                echo "Error: catatan gagal dibuat!"
-            fi
+            make_note
             ;;
         5)
-            read -p "Masukkan file yang ingin dibaca: " fileDicari
-            cat $fileDicari
+            read_file
             ;;
         6)
             calculator
             ;;
         7)
-            allQuotes=("Don’t compare yourself with anyone in this world. If you do so, you are insulting yourself. - Bill Gates" "Invention requires a long-term willingness to be misunderstood. - Jeff Bezos" "Persistence is very important. You should not give up unless you are forced to give up. - Elon Musk" "The only way to do great work is to love what you do. - Steve Wozniak" "The way to do really big things seems to be to start with deceptively small things.  - Paul Graham")
-
-            currentSecond=$(date +%S)
-            secondModulo5=$((currentSecond % 5))
-            case $secondModulo5 in
-                0)
-                    echo -e "\n\033[0;33m${allQuotes[0]}\033[0m"
-                    ;;
-                1)
-                    echo -e "\n\033[0;33m${allQuotes[1]}\033[0m"
-                    ;;
-                2)
-                    echo -e "\n\033[0;33m${allQuotes[2]}\033[0m"
-                    ;;
-                3)
-                    echo -e "\n\033[0;33m${allQuotes[3]}\033[0m"
-                    ;; 
-                4)
-                    echo -e "\n\033[0;33m${allQuotes[4]}\033[0m"
-                    ;;
-            esac
+            give_motivation
             ;;
         *)
             echo "Pilihan tidak valid."
